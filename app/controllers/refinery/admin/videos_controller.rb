46544042ae96d6b56e6146d3538069ea77cc514b
Jamie Winsor <jamie@enmasse.com>
2011-08-30 17:09:07 -0700

    Refactor video upload to allow for uploads to come from another source other than NGINX

--- app/controllers/refinery/admin/videos_controller.rb
+++ app/controllers/refinery/admin/videos_controller.rb
@@ -1,6 +1,8 @@
 module Refinery
   module Admin
     class VideosController < ::Admin::BaseController
+      
+      respond_to :html
 
       crudify :'refinery/video',
               :title_attribute => 'title',
@@ -8,12 +10,27 @@ module Refinery
               :sortable => false
             
       def create
-        @video = Video.create_from_nginx_upload(params[:video])
-        @video.async_encode(:mp4, :ogv, :webm)
-      
-        redirect_to main_app.refinery_admin_videos_path, :notice => "Video successfully created. Encoding process will start shortly."
+        if nginx_upload?
+          @video = Video.create_from_nginx_upload(params[:video])
+        else
+          @video = Video.create(params[:video])
+        end
+        
+        if @video
+          @video.async_encode(:mp4, :ogv, :webm)
+          flash[:notice] = t('encoding', :scope => 'refinery.admin.videos')
+        end
+        
+        respond_with [:refinery_admin, @video], :location => main_app.refinery_admin_videos_path
       end
       alias_method :upload, :create
     end
+    
+    protected
+      
+      def nginx_upload?
+        params[:video][:path] && params[:video][:file_name]
+      end
+
   end
 end
--- app/models/refinery/video.rb
+++ app/models/refinery/video.rb
@@ -13,7 +13,7 @@ module Refinery
     delegate :name, :mime_type, :height, :width, :ext, :resolution, :to => :raw
   
     acts_as_indexed :fields => [:name]
-  
+
     def self.create_from_nginx_upload(params)
       video_path = File.join(File.dirname(params[:path]), params[:file_name])
       FileUtils.mv(params[:path], video_path)
@@ -26,9 +26,7 @@ module Refinery
     end
     
     def async_encode(*formats)
-      formats.each do |format|
-        Resque.enqueue(Refinery::EncodeVideo, self.id, format)
-      end
+      formats.each { |format| Resque.enqueue(Refinery::EncodeVideo, self.id, format) }
     end
   
     # Returns a titleized version of the filename
