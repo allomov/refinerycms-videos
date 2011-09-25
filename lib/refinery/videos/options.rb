f172cf8cd562df2350151a55fd56843733aa6767
Jamie Winsor <jamie@enmasse.com>
2011-09-24 20:53:45 -0700

    Add upload progress URI path configuration option

--- lib/refinery/videos/options.rb
+++ lib/refinery/videos/options.rb
@@ -4,13 +4,18 @@ module Refinery
       include Rails::Railtie::Configurable
 
       DEFAULT_USE_NGINX_UPLOAD_MODULE = false
+      DEFAULT_UPLOAD_PROGRESS_URI = '/progress'
 
       cattr_accessor :use_nginx_upload_module
       self.use_nginx_upload_module = DEFAULT_USE_NGINX_UPLOAD_MODULE
+      
+      cattr_accessor :upload_progress_uri
+      self.upload_progress_uri = DEFAULT_UPLOAD_PROGRESS_URI
 
       class << self
         def reset!
           self.use_nginx_upload_module = DEFAULT_USE_NGINX_UPLOAD_MODULE
+          self.upload_progress_uri = DEFAULT_UPLOAD_PROGRESS_URI
         end
       end
     end
--- spec/lib/refinery/videos/options_spec.rb
+++ spec/lib/refinery/videos/options_spec.rb
@@ -10,6 +10,14 @@ module Refinery
         subject.class.reset!
         subject.class.use_nginx_upload_module.should == subject.class::DEFAULT_USE_NGINX_UPLOAD_MODULE
       end
+      
+      it "should set upload_progress_uri back to the default value" do      
+        subject.class.upload_progress_uri.should == subject.class::DEFAULT_UPLOAD_PROGRESS_URI
+        subject.class.upload_progress_uri = '/something/progress'
+        subject.class.upload_progress_uri.should_not == subject.class::DEFAULT_UPLOAD_PROGRESS_URI
+        subject.class.reset!
+        subject.class.upload_progress_uri.should == subject.class::DEFAULT_UPLOAD_PROGRESS_URI
+      end
     end
   end
 end
