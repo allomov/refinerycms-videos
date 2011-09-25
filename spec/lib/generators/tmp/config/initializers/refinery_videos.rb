f6eb757dcbab736c76bd45921042a7c97e018186
Jamie Winsor <jamie@enmasse.com>
2011-09-24 20:53:34 -0700

    Remove tmp directories

--- spec/lib/generators/tmp/config/initializers/refinery_videos.rb
+++
@@ -1,4 +0,0 @@
-Refinery::Videos::Options.configure do |config|
-  # Configures the maximum allowed upload size (in bytes) for an image
-  # config.use_nginx_upload_module = false
-end
--- spec/lib/generators/tmp/db/migrate/20110925022226_add_poster_image_to_raw_video.rb
+++
@@ -1,5 +0,0 @@
-class AddPosterImageToRawVideo < ActiveRecord::Migration
-  def change
-    add_column :refinery_raw_videos, :poster_image_id, :integer
-  end
-end
